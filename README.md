b868fe9e58bd6c61af0d3579b931de9f2df4905d
ric cox <ric.cox@gmail.com>
2021-01-24 19:10:10 -0700

    initial

---
+++ README.md
@@ -0,0 +1,46 @@
+### lookup
+
+Succinct DNS record lookup. I needed simple terse DNS record information for a bash script. This worked.
+
+     > go run lookup.go -a www.golang.org
+     www.golang.org	A	 172.217.4.177
+     www.golang.org	A	 2607:f8b0:4007:80e::2011
+     >
+
+     > go run lookup.go -cname www.cisco.com
+     www.cisco.com	CNAME	 e2867.dsca.akamaiedge.net.
+     >
+
+     > go run lookup.go -mx cox.com
+     cox.com	MX	mxa-002b3901.gslb.pphosted.com.	 10
+     cox.com	MX	mxb-002b3901.gslb.pphosted.com.	 10
+     >
+
+     > go run lookup.go -ns google.com
+     google.com	NS	ns3.google.com.
+     google.com	NS	ns2.google.com.
+     google.com	NS	ns4.google.com.
+     google.com	NS	ns1.google.com.
+     >
+
+     > go run lookup.go -ptr 216.239.32.10
+     216.239.32.10	PTR	ns1.google.com.
+     >
+
+     > go run lookup.go -srv spud.com -s xmpp-server -p tcp
+     lookup _xmpp-server._tcp.spud.com on 127.0.0.53:53: no such host
+     >
+
+     > go run lookup.go -txt costco.com
+     costco.com	TXT	have-i-been-pwned-verification=7411effeb11c12300a4c027396b4cf0f
+     costco.com	TXT	docusign=3e51deb4-b2f9-46ef-b120-18f6955ebce1
+     costco.com	TXT	MS=ms45356090
+     costco.com	TXT	docusign=4db9a87a-9dea-417f-bfaa-d45bf7c94291
+     costco.com	TXT	ciscocidomainverification=778c9d75e8b6fdb7dc3f095b677d08d21ececebe8462cb691c377eb0c061c825
+     costco.com	TXT	facebook-domain-verification=46if6nzbzbffbhikh3uq8sye7ez5bg
+     costco.com	TXT	google-site-verification=dffTnhrKhs3V5-UUHvEYJg-RNLJjBz27jCBqmF6sX_E
+     costco.com	TXT	google-site-verification=3Kx9tc2_v5CgX3NuVqNwWFZRYanPPJCP3__8KbRxn5Q
+     costco.com	TXT	adobe-idp-site-verification=ae9e3f0f-2848-4000-aed4-e1c7e8031815
+     costco.com	TXT	v=spf1 include:_spf.costco.com include:_spf.google.com include:_spf1.costco.com      include:_spf2.costco.com include:_spf3.costco.com include:_spf4.costco.com include:spf-001f9d01.pphosted.com -all
+
+>
---
+++ lookup.go
@@ -0,0 +1,103 @@
+/*
+ *   lookup.go - Short quick DNS record lookup.  Perfect for use in a bash script.
+ *
+ *   v 1.0
+ */
+
+package main
+
+import (
+  "context"
+  "flag"
+  "fmt"
+  "net"
+  "time"
+)
+
+var a_rec, cname, mx, ns, ptr, srv, proto, serv, txt, help string
+
+func init() {
+  flag.StringVar(&a_rec, "a",     "", "A record lookup. Hostname required")
+  flag.StringVar(&cname, "cname", "", "CNAME record lookup. Hostname required")
+  flag.StringVar(&mx,    "mx",    "", "MX record lookup. Domain name required")
+  flag.StringVar(&ns,    "ns",    "", "NS record lookup. Domain name required")
+  flag.StringVar(&ptr,   "ptr",   "", "PTR record lookup. IP address required {IPv4 or IPv6}")
+  flag.StringVar(&srv,   "srv",   "", "Domain name required. See -p and -s")
+  flag.StringVar(&proto, "p",     "", "Protocol required {tcp or udp}.  See -srv")
+  flag.StringVar(&serv,  "s",     "", "Service required {e.g., xmpp-server}. See -srv")
+  flag.StringVar(&txt,   "txt",   "", "Domain name required")
+  flag.StringVar(&help,  "help",  "", "Show usage")
+}
+
+func main() {
+  flag.Parse()
+
+  // Sometimes nameservers timeout
+  ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+  defer cancel()
+
+  select {
+  case <- time.After(1 * time.Second):
+            fmt.Println("timeout")
+  case <- ctx.Done():
+            fmt.Println(ctx.Err())
+  default:
+
+    if a_rec != "" {                               // A
+      list, err := net.LookupIP(a_rec)
+      if ( err != nil ) { fmt.Println(err) }
+      for _, ip := range list {
+        fmt.Println(a_rec + "\tA\t", ip)
+      }
+    } else if cname != ""  {                       // CNAME
+      name, err := net.LookupCNAME(cname)
+      if ( err != nil ) { fmt.Println(err) }
+      fmt.Println(cname + "\tCNAME\t", name)
+    } else if mx != ""  {                          // MX
+      mx_records, err := net.LookupMX(mx)
+      if ( err != nil ) { fmt.Println(err) }
+      for _, record := range mx_records {
+        fmt.Println(mx + "\tMX\t" + record.Host + "\t", record.Pref)
+      }
+    } else if ns != ""  {                          // NS
+      ns_records, err := net.LookupNS(ns)
+      if ( err != nil ) { fmt.Println(err) }
+      for _, record := range ns_records {
+        fmt.Println(ns + "\tNS\t" + record.Host)
+      }
+    } else if ptr != ""  {                         // PTR
+      names, err := net.LookupAddr(ptr)
+      if ( err != nil ) { fmt.Println(err) }
+      for _, name := range names {
+        fmt.Println(ptr + "\tPTR\t" + name)
+      }
+    } else if srv != ""  {                         // SRV
+      if proto != "" && serv != "" {
+        name, services, err := net.LookupSRV(serv, proto, srv)
+        for _, service := range services {
+          fmt.Printf("%s\t%s\t%v\t%v\t%d\t%d\n", srv, name, service.Target, service.Port, service.Priority, service.Weight)
+        }
+        if ( err == nil ) {
+        } else {
+          fmt.Println(err)
+        }
+      } else {
+        fmt.Println("service (-s), protocol (-p) and domain (-d) required")
+      }
+    } else if txt != ""  {                         // TXT
+      records, err := net.LookupTXT(txt)
+      if ( err != nil ) { fmt.Println(err) }
+      for _, record := range records {
+        fmt.Println(txt + "\tTXT\t" + record)
+      }
+    } else if help == ""  {                         // Usage
+      fmt.Println("\nNAME\n    lookup - succinct DNS record retrieval\n")
+      fmt.Println("SYNOPSIS\n    lookup [OPTION]\n")
+      fmt.Println("DESCRIPTION\n    lookup [OPTION]\n")
+      flag.PrintDefaults()
+      fmt.Println()
+    } else {
+      fmt.Println("Try \"lookup -help\"")
+    }
+  }
+}
